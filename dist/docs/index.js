e3535ced2f7c62e421f35f8a9a34694ed18d66bb
Pixoll <pixoll.gamer@gmail.com>
2024-10-20 19:09:25 -0300

    Remove docs directory

--- dist/docs/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MethodDocs = void 0;
-var decorators_1 = require("./decorators");
-Object.defineProperty(exports, "MethodDocs", { enumerable: true, get: function () { return decorators_1.MethodDocs; } });
-//# sourceMappingURL=index.js.map
