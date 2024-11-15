15005ae81118c40ddac95d45077572e9ad4a04fe
Pixoll <pixoll.gamer@gmail.com>
2024-11-15 14:42:09 -0300

    Create GET /nutrients

--- src/endpoints/v1/index.ts
+++ src/endpoints/v1/index.ts
@@ -2,6 +2,7 @@ export { AdminsEndpoint } from "./admins";
 export { FoodsEndpoint } from "./foods";
 export { GroupsEndpoint } from "./groups";
 export { LanguagesEndpoint } from "./languages";
+export { NutrientsEndpoint } from "./nutrients";
 export { OriginsEndpoint } from "./origins";
 export { PingEndpoint } from "./ping";
 export { TypesEndpoint } from "./types";
