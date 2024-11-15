ad54db8bbba08760f79408171c0e166d8098032c
Pixoll <pixoll.gamer@gmail.com>
2024-11-15 00:27:15 -0300

    Create GET /groups and GET /types

--- src/endpoints/v1/index.ts
+++ src/endpoints/v1/index.ts
@@ -1,4 +1,6 @@
 export { AdminsEndpoint } from "./admins";
 export { FoodsEndpoint } from "./foods";
+export { GroupsEndpoint } from "./groups";
 export { OriginsEndpoint } from "./origins";
 export { PingEndpoint } from "./ping";
+export { TypesEndpoint } from "./types";
