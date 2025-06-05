fb3bbd78d6bcc29da28baf4e1e82f6379cb1060b
Pixoll <pixoll.gamer@gmail.com>
2025-06-05 00:59:08 -0400

    fix: only use method key as OpenAPI identifier

--- src/main.ts
+++ src/main.ts
@@ -47,6 +47,7 @@ void async function () {
 
     SwaggerModule.setup(globalPrefix, app, () => SwaggerModule.createDocument(app, swaggerConfig, {
         ignoreGlobalPrefix: true,
+        operationIdFactory: (_controllerKey: string, methodKey: string) => methodKey,
     }));
 
     await app.listen(process.env.PORT ?? 3000);
