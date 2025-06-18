5ab73f718a7152405a5f7bafe7d64ad1524361d3
Pixoll <pixoll.gamer@gmail.com>
2025-06-18 13:02:34 -0400

    fix: use value from constants.ts

--- src/modules/auth/decorators/auth-cookie.decorator.ts
+++ src/modules/auth/decorators/auth-cookie.decorator.ts
@@ -1,9 +1,9 @@
 import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 import { Request } from "express";
+import { AUTH_COOKIE_NAME } from "../constants";
 
 export const AuthCookie = createParamDecorator((_: unknown, ctx: ExecutionContext): string | undefined => {
-    const { AUTH_COOKIE_NAME } = process.env;
     const request = ctx.switchToHttp().getRequest<Request>();
-    const cookie = request.signedCookies?.[AUTH_COOKIE_NAME ?? ""] as false | string | undefined;
+    const cookie = request.signedCookies?.[AUTH_COOKIE_NAME] as false | string | undefined;
     return cookie || undefined;
 });
